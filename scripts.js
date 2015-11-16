d8c0e026852478cd774aabe97ea0b5676755f5a3
Ben.Pappas <mrbenpappas@gmail.com>
2015-11-16 00:43:29 -0500

    added more currencies

--- scripts.js
+++ scripts.js
@@ -7,10 +7,27 @@ function ajaxCall() {
 			var eur = (data.EUR.last).toFixed(2);
 			var gbp = (data.GBP.last).toFixed(2);
 			var cny = (data.CNY.last).toFixed(2);
-			$('#usd-result').html('$' + usd);
+			var jpy = (data.JPY.last).toFixed(2);
+			var cad = (data.CAD.last).toFixed(2);
+			var aud = (data.AUD.last).toFixed(2);
+			var chf = (data.CHF.last).toFixed(2);
+			var nzd = (data.NZD.last).toFixed(2);
+			var sek = (data.SEK.last).toFixed(2);
+			var rub = (data.RUB.last).toFixed(2);
+			var brl = (data.BRL.last).toFixed(2);
+			var dollar = '<i class="fa fa-usd"></i>';
+			$('#usd-result').html(dollar + usd);
 			$('#eur-result').html('<i class="fa fa-eur"></i>' + eur);
 			$('#gbp-result').html('<i class="fa fa-gbp"></i>' + gbp);
-			$('#cny-result').html('CNY ' + cny);
+			$('#cny-result').html('<i class="fa fa-jpy"></i>' + cny);
+			$('#jpy-result').html('<i class="fa fa-jpy"></i>' + jpy);
+			$('#cad-result').html(dollar + cad);
+			$('#aud-result').html(dollar + aud);
+			$('#chf-result').html('CHF ' + chf);
+			$('#nzd-result').html(dollar + nzd);
+			$('#sek-result').html(sek + ' kr');
+			$('#rub-result').html('<i class="fa fa-rub"></i>' + rub);
+			$('#brl-result').html(dollar + brl);
 			//set title to USD
 			$('title').html('$'+usd);
 		}
